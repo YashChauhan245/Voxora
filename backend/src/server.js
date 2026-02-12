e06d510dcd1d359d03593eb98fa354ed22b3cf05
YashChauhan245 <yashchau.work@gmail.com>
2026-02-12 17:35:50 +0530

    chore: initial project setup

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import "dotenv/config";
+import authRoutes from "./routes/auth.route.js";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
+
+
+app.listen(5001, () => {
+  console.log(`server is running on ${PORT}`);
+});
