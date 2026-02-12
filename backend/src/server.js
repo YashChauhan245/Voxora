089703f217a9ef4803448478698daa4d5a1e2647
YashChauhan245 <yashchau.work@gmail.com>
2026-02-12 19:24:46 +0530

    feat: setup Stream integration and database configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -10,4 +11,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(5001, () => {
   console.log(`server is running on ${PORT}`);
-});
+  connectDB(); 
+}); 
