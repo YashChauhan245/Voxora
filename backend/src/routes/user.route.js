fd022bc72053a915463752c433621bea9b738540
YashChauhan245 <yashchau.work@gmail.com>
2026-02-13 20:26:03 +0530

    feat: implement friend request system with send, accept,  get and outgoing status endpoints

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import {
+  acceptFriendRequest,
+  getFriendRequests,
   getMyFriends,
+  getOutgoingFriendReqs,
   getRecommendedUsers,
+  sendFriendRequest,
 } from "../controllers/user.controller.js";
+
 
 const router = express.Router();
 
@@ -13,5 +18,10 @@ router.use(protectRoute);
 router.get("/", getRecommendedUsers);
 router.get("/friends", getMyFriends);
 
+router.post("/friend-request/:id", sendFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest); //put as we the updating the fields
+
+router.get("/friend-requests", getFriendRequests);
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
 
 export default router;
