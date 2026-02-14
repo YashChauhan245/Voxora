09538df2b3a07e2a6306820dad4f15c967de19fb
YashChauhan245 <yashchau.work@gmail.com>
2026-02-14 17:27:39 +0530

    feat: add signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,46 @@
 import React from 'react'
 import HomePage from './pages/HomePage'
-import { Routes, Route } from "react-router";
+import { Routes, Route ,Navigate } from "react-router";
+import { axiosInstance } from './lib/axios';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import { useQuery } from "@tanstack/react-query";
 
 const App = () => {
+  const {
+    data: authData,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry: false, // auth check
+  });
+
+  const authUser = authData?.user;
+
   return (
-    <div className='bg-red-500 h-screen text-5xl'>App
-     <Routes>
-      <Route path="/" element={<HomePage/>}/>
-     </Routes>
-    </div>
-  )
+      <div className="h-screen" data-theme="night">
+        <Routes>
+          <Route
+            path="/"
+            element={authUser ? <HomePage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/signup"
+            element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          />
+        </Routes>
+      </div>
+    );
+
 }
 
 export default App
